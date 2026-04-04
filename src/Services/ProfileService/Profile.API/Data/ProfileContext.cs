977e247e75fddf23ea4d7c9759982dbae861a3a7
David Toholj <davidtoholj0101@gmail.com>
2026-04-04 16:41:11 +0200

    Create Database context
    Refs #5

---
+++ src/Services/ProfileService/Profile.API/Data/ProfileContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Profile.API.Entities;
+
+namespace Profile.API.Data
+{
+    public class ProfileContext : DbContext, IProfileContext
+    {
+        public ProfileContext(DbContextOptions<ProfileContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<CandidateProfile> CandidateProfiles { get; set; }
+        public DbSet<CompanyProfile> CompanyProfiles { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
