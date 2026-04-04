503ee1ba4f7fe788d095ff8e8478eaada575f82e
Laki27 <lazardackovic@gmail.com>
2026-04-04 15:22:59 +0200

    feat: initial chat service implementation

---
+++ src/Services/Chat.API/Models/Message.cs
@@ -0,0 +1,24 @@
+﻿
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Chat.API.Models
+{
+
+    public class Message
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string ChatId { get; set; }
+
+        public string SenderId { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+    }
+
+}
---
+++ src/Services/Chat.API/Program.cs
@@ -0,0 +1,23 @@
+using Chat.API.Services;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSingleton<ChatService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(); 
+}
+
+app.MapControllers();
+
+app.Run();
