3637f0429afc1b2e5f62bad66a1e5f79efb514e1
Petar <petarlukovic17@gmail.com>
2026-04-05 15:40:39 +0200

    feat: Add Job entity with MongoDB annotations and default values. #9

--- src/Services/Catalog/Entities/Job.cs
+++ src/Services/Catalog/Entities/Job.cs
@@ -1,6 +1,22 @@
+using MongoDB.Bson;
+using  MongoDB.Bson.Serialization.Attributes;
+
 namespace Catalog.Entities;
 
 public class Job
 {
     
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public string CompanyId { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+
+    public decimal Salary { get; set; }
+    public string Location { get; set; } = string.Empty;
+
+    public DateTime PostedDate { get; set; } = DateTime.UtcNow;
 }
