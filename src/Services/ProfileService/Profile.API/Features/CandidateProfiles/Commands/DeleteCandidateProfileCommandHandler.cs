536c3ee347596fb269194e07a20fc26e6675f0de
David Toholj <davidtoholj0101@gmail.com>
2026-04-12 22:01:51 +0200

    Add DeleteCandidateProfile command and handler
    Refs #17

---
+++ src/Services/ProfileService/Profile.API/Features/CandidateProfiles/Commands/DeleteCandidateProfileCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Profile.API.Data;
+
+namespace Profile.API.Features.CandidateProfiles.Commands
+{
+    public class DeleteCandidateProfileCommandHandler(IProfileContext context, ILogger<DeleteCandidateProfileCommandHandler> logger) : IRequestHandler<DeleteCandidateProfileCommand, bool>
+    {
+        public async Task<bool> Handle(DeleteCandidateProfileCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await context.CandidateProfiles.FirstOrDefaultAsync(p => p.Id == request.Id, cancellationToken);
+
+            if(entity == null)
+            {
+                logger.LogWarning("Candidate profile {ProfileId} not found.", request.Id);
+                return false;
+            }
+
+            context.CandidateProfiles.Remove(entity);
+            await context.SaveChangesAsync(cancellationToken);
+
+            logger.LogInformation("Successfully deleted candidate profile {ProfileId}.", request.Id);
+
+            return true;
+        }
+    }
+}
