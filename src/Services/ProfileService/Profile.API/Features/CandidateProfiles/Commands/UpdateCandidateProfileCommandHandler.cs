2b35b1cdaffb80efae634ecf590300205f3c5409
David Toholj <davidtoholj0101@gmail.com>
2026-04-12 21:50:57 +0200

    Add UpdateCandidateProfile command and handler
    Refs #17

---
+++ src/Services/ProfileService/Profile.API/Features/CandidateProfiles/Commands/UpdateCandidateProfileCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Profile.API.Data;
+
+namespace Profile.API.Features.CandidateProfiles.Commands
+{
+    public class UpdateCandidateProfileCommandHandler(IProfileContext context, IMapper mapper, ILogger<UpdateCandidateProfileCommandHandler> logger) : IRequestHandler<UpdateCandidateProfileCommand, bool>
+    {
+        public async Task<bool> Handle(UpdateCandidateProfileCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await context.CandidateProfiles.FirstOrDefaultAsync(p => p.Id == request.Id, cancellationToken);
+
+            if(entity == null)
+            {
+                logger.LogWarning("Candidate profile {ProfileId} not found.", request.Id);
+                return false;
+            }
+
+            mapper.Map(request, entity);
+
+            await context.SaveChangesAsync(cancellationToken);
+            logger.LogInformation("Successfully updated candidate profile {ProfileId}", request.Id);
+
+            return true;
+        }
+    }
+}
