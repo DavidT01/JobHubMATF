36d2790f67e0ab9f4d6423de2c89920af9884994
David Toholj <davidtoholj0101@gmail.com>
2026-04-13 23:09:55 +0200

    Add GetCandidateProfile query and handler
    Refs #17

---
+++ src/Services/ProfileService/Profile.API/Features/CandidateProfiles/Queries/GetCandidateProfileQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Profile.API.DTO;
+
+namespace Profile.API.Features.CandidateProfiles.Queries
+{
+    public record GetCandidateProfileQuery(string UserId) : IRequest<CandidateProfileDto?>;
+}
