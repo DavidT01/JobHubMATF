0ca9474b3f1383803884cc1367a748cda1fafc79
David Toholj <davidtoholj0101@gmail.com>
2026-04-13 23:09:55 +0200

    Add GetCandidateProfile query and handler
    Refs #17

---
+++ src/Services/ProfileService/Profile.API/Features/CandidateProfiles/Queries/GetCandidateProfileQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Profile.API.Data;
+using Profile.API.DTO;
+
+namespace Profile.API.Features.CandidateProfiles.Queries
+{
+    public class GetCandidateProfileQueryHandler(IProfileContext context, IMapper mapper, ILogger<GetCandidateProfileQueryHandler> logger) : IRequestHandler<GetCandidateProfileQuery, CandidateProfileDto?>
+    {
+public async Task<CandidateProfileDto?> Handle(GetCandidateProfileQuery request, CancellationToken cancellationToken)
+        {
+            var profile = await context.CandidateProfiles.FirstOrDefaultAsync(p => p.UserId == request.UserId, cancellationToken);
+
+            if(profile == null)
+            {
+                logger.LogWarning("Candidate profile {UserId} not found.", request.UserId);
+                return null;
+            }
+
+            return mapper.Map<CandidateProfileDto>(profile);
+        }
+    }
+}
