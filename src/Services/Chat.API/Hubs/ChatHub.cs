9aa952e06ec4a21bc11db60b0e0fd0e0ca24ca67
Laki27 <lazardackovic@gmail.com>
2026-04-15 21:43:45 +0200

    Real-time chat backend

---
+++ src/Services/Chat.API/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Chat.API.Services;
+using Microsoft.AspNetCore.SignalR;
+namespace Chat.API.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly ChatService _chatService;
+
+        public ChatHub(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+        public async Task SendMessage(string senderId, string reciverId, string message)
+        {
+            var savedMessage = await _chatService.SendMessageAsync(senderId, reciverId, message);
+            await Clients.All.SendAsync("ReceiveMessage", 
+                    savedMessage.SenderId,
+                    reciverId,
+                    savedMessage.Text,
+                    savedMessage.Timestamp);
+        }
+    }
+}
--- src/Services/Chat.API/Program.cs
+++ src/Services/Chat.API/Program.cs
@@ -1,6 +1,6 @@
 using Chat.API.Services;
 using Scalar.AspNetCore;
-
+using Chat.API.Hubs;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<ChatService>();
+builder.Services.AddSignalR();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,5 +20,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
