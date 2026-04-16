a12f3e68bfc6bae188c0cf8651413c3abcad5947
David Toholj <davidtoholj0101@gmail.com>
2026-04-16 22:28:03 +0200

    Add extension for validation
    Extension consists of a function that checks if URL is valid, which is used both for `CandidateProfile` and `CompanyProfile`
    Refs #18

---
+++ src/Services/ProfileService/Profile.API/Extensions/ValidationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Profile.API.Extensions
+{
+    public static class ValidationExtensions
+    {
+        public static IRuleBuilderOptions<T, string?> MustBeValidUrl<T>(this IRuleBuilder<T, string?> ruleBuilder)
+        {
+            return ruleBuilder.Must(url =>
+            {
+                if (string.IsNullOrWhiteSpace(url))
+                    return true;
+
+                return Uri.TryCreate(url, UriKind.Absolute, out Uri? outUri)
+                    && (outUri.Scheme == Uri.UriSchemeHttp || outUri.Scheme == Uri.UriSchemeHttps);
+            }).WithMessage("'{Link}' must be a valid HTTP or HTTPS URL.");
+        }
+    }
+}
