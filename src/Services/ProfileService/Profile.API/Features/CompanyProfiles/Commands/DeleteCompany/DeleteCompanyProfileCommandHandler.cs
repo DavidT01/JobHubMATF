ecfbcc2096de0e41a427dd5c62f7ab63990e3110
David Toholj <davidtoholj0101@gmail.com>
2026-04-16 21:34:46 +0200

    Add DeleteCompanyProfile command and handler
    Refs #18

---
+++ src/Services/ProfileService/Profile.API/Features/CompanyProfiles/Commands/DeleteCompany/DeleteCompanyProfileCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Profile.API.Data;
+using Profile.API.Features.CompanyProfiles.Commands.CreateCompany;
+
+namespace Profile.API.Features.CompanyProfiles.Commands.DeleteCompany
+{
+    public class DeleteCompanyProfileCommandHandler(IProfileContext context, ILogger<DeleteCompanyProfileCommandHandler> logger) : IRequestHandler<DeleteCompanyProfileCommand, bool>
+    {
+        public async Task<bool> Handle(DeleteCompanyProfileCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await context.CompanyProfiles.FirstOrDefaultAsync(p => p.Id == request.Id, cancellationToken);
+
+            if (entity == null)
+            {
+                logger.LogWarning("Company profile {ProfileId} not found.", request.Id);
+                return false;
+            }
+
+            context.CompanyProfiles.Remove(entity);
+            await context.SaveChangesAsync(cancellationToken);
+
+            logger.LogInformation("Successfully deleted company profile {ProfileId}.", request.Id);
+
+            return true;
+        }
+    }
+}
