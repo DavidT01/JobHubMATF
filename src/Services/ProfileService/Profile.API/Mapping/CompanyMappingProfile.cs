6c38b5cea9174a49576de6b3fd8209b69fa19e10
David Toholj <davidtoholj0101@gmail.com>
2026-04-16 21:43:02 +0200

    Add UpdateCompanyProfile command, handler and mapping
    Refs #18

--- src/Services/ProfileService/Profile.API/Mapping/CompanyMappingProfile.cs
+++ src/Services/ProfileService/Profile.API/Mapping/CompanyMappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using Profile.API.DTOs;
 using Profile.API.Entities;
 using Profile.API.Features.CompanyProfiles.Commands.CreateCompany;
+using Profile.API.Features.CompanyProfiles.Commands.UpdateCompany;
 
 namespace Profile.API.Mapping
 {
@@ -11,9 +12,15 @@ namespace Profile.API.Mapping
             CreateMap<CompanyProfile, CompanyProfileDto>();
 
             CreateMap<CreateCompanyProfileCommand, CompanyProfile>()
-            .ForMember(dest => dest.Id, opt => opt.Ignore())
-            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
-            .ForMember(dest => dest.ModifiedAt, opt => opt.Ignore());
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.ModifiedAt, opt => opt.Ignore());
+
+            CreateMap<UpdateCompanyProfileCommand, CompanyProfile>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.ModifiedAt, opt => opt.Ignore());
         }
     }
 }
