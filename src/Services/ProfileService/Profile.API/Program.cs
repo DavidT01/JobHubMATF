6462161b490837a5b301d56f14d52e603ee1534e
David Toholj <davidtoholj0101@gmail.com>
2026-04-16 18:52:16 +0200

    Add MediatR validation pipeline, exception and exception handler
    Refs #17

--- src/Services/ProfileService/Profile.API/Program.cs
+++ src/Services/ProfileService/Profile.API/Program.cs
@@ -1,6 +1,8 @@
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Profile.API.Data;
+using Profile.API.Exceptions;
+using Profile.API.Features.Behaviors;
 using Scalar.AspNetCore;
 using System.Reflection;
 
@@ -12,6 +14,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
+builder.Services.AddProblemDetails();
+
 var connectionString = builder.Configuration.GetConnectionString("ProfileDbConnection") ?? throw new InvalidOperationException("Connection string 'ProfileDbConnection' is not valid.");
 builder.Services.AddDbContext<ProfileContext>(options => options.UseNpgsql(connectionString));
 
@@ -19,11 +24,16 @@ builder.Services.AddScoped<IProfileContext, ProfileContext>();
 
 builder.Services.AddAutoMapper(cfg => cfg.AddMaps(Assembly.GetExecutingAssembly()));
 
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddMediatR(cfg => {
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    });
 
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
 var app = builder.Build();
+
+app.UseExceptionHandler();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
