b6c170df37d8bd35f274acaf3bd03c38e0f61dc3
David Toholj <davidtoholj0101@gmail.com>
2026-04-16 20:24:32 +0200

    Add mapping for CompanyProfileDto
    Refs #18

---
+++ src/Services/ProfileService/Profile.API/Mapping/CompanyMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using Profile.API.DTO;
+using Profile.API.DTOs;
+using Profile.API.Entities;
+using Profile.API.Features.CandidateProfiles.Commands.CreateCandidate;
+using Profile.API.Features.CandidateProfiles.Commands.UpdateCandidate;
+
+namespace Profile.API.Mapping
+{
+    public class CompanyMappingProfile : AutoMapper.Profile
+    {
+        public CompanyMappingProfile()
+        {
+            CreateMap<CompanyProfile, CompanyProfileDto>();
+        }
+    }
+}
