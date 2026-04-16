ac0d3a1317751daf759a26dd0217dfa209bed6b4
David Toholj <davidtoholj0101@gmail.com>
2026-04-16 21:03:33 +0200

    Add CreateCompanyProfile command, handler and mapping
    Refs #18

---
+++ src/Services/ProfileService/Profile.API/Features/CompanyProfiles/Commands/CreateCompany/CreateCompanyProfileCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Profile.API.Features.CompanyProfiles.Commands.CreateCompany
+{
+    public class CreateCompanyProfileCommand : IRequest<Guid>
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string ContactEmail { get; set; } = string.Empty;
+        public string ContactPhone { get; set; } = string.Empty;
+        public string? WebsiteUrl { get; set; }
+        public string? LinkedInUrl { get; set; }
+        public string? LogoUrl { get; set; }
+    }
+}
