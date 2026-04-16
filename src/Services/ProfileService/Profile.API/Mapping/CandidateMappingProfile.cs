1a0d1d58d6b7f4e1c1806bfd5c993b8578aa70c0
David Toholj <davidtoholj0101@gmail.com>
2026-04-16 21:43:02 +0200

    Add UpdateCompanyProfile command, handler and mapping
    Refs #18

--- src/Services/ProfileService/Profile.API/Mapping/CandidateMappingProfile.cs
+++ src/Services/ProfileService/Profile.API/Mapping/CandidateMappingProfile.cs
@@ -3,23 +3,24 @@ using Profile.API.Entities;
 using Profile.API.Features.CandidateProfiles.Commands.CreateCandidate;
 using Profile.API.Features.CandidateProfiles.Commands.UpdateCandidate;
 
-namespace Profile.API.Mapping;
-
-public class CandidateMappingProfile : AutoMapper.Profile
+namespace Profile.API.Mapping
 {
-    public CandidateMappingProfile()
+    public class CandidateMappingProfile : AutoMapper.Profile
     {
-        CreateMap<CandidateProfile, CandidateProfileDto>();
+        public CandidateMappingProfile()
+        {
+            CreateMap<CandidateProfile, CandidateProfileDto>();
 
-        CreateMap<CreateCandidateProfileCommand, CandidateProfile>()
-            .ForMember(dest => dest.Id, opt => opt.Ignore())
-            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
-            .ForMember(dest => dest.ModifiedAt, opt => opt.Ignore());
+            CreateMap<CreateCandidateProfileCommand, CandidateProfile>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.ModifiedAt, opt => opt.Ignore());
 
-        CreateMap<UpdateCandidateProfileCommand, CandidateProfile>()
-            .ForMember(dest => dest.Id, opt => opt.Ignore())
-            .ForMember(dest => dest.UserId, opt => opt.Ignore())
-            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
-            .ForMember(dest => dest.ModifiedAt, opt => opt.Ignore());
+            CreateMap<UpdateCandidateProfileCommand, CandidateProfile>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.ModifiedAt, opt => opt.Ignore());
+        }
     }
 }
