61e89fa901a74216e6d6624cdfeaabc9529f27f2
David Toholj <davidtoholj0101@gmail.com>
2026-04-16 19:19:13 +0200

    Force unique UserId on CandidateProfile
    Refs #17

--- src/Services/ProfileService/Profile.API/Data/ProfileContext.cs
+++ src/Services/ProfileService/Profile.API/Data/ProfileContext.cs
@@ -14,6 +14,13 @@ namespace Profile.API.Data
         public DbSet<CandidateProfile> CandidateProfiles { get; set; } = null!;
         public DbSet<CompanyProfile> CompanyProfiles { get; set; } = null!;
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CandidateProfile>().HasIndex(p => p.UserId).IsUnique();
+        }
+
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             return base.SaveChangesAsync(cancellationToken);
