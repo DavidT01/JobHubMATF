951dcb57336295b2617797ea461b1f4bf746083c
David Toholj <davidtoholj0101@gmail.com>
2026-04-19 12:28:15 +0200

    Remove Guid initialization from `CompanyProfileDto`
    Refs #18

--- src/Services/ProfileService/Profile.API/DTOs/CompanyProfileDto.cs
+++ src/Services/ProfileService/Profile.API/DTOs/CompanyProfileDto.cs
@@ -2,7 +2,7 @@
 {
     public class CompanyProfileDto
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid Id { get; set; }
         public string UserId { get; set; } = string.Empty;
         public string CompanyName { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
