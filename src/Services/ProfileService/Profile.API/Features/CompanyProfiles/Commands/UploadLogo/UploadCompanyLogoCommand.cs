e7dee296ae260c2e24948bb2d4cf6115d0afd7a9
David Toholj <davidtoholj0101@gmail.com>
2026-04-19 13:51:28 +0200

    Add `UploadCompanyLogo` command, handler and validator
    Refs #20

---
+++ src/Services/ProfileService/Profile.API/Features/CompanyProfiles/Commands/UploadLogo/UploadCompanyLogoCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Profile.API.Features.CompanyProfiles.Commands.UploadLogo
+{
+    public class UploadCompanyLogoCommand : IRequest<string?>
+    {
+        public Guid Id { get; set; }
+        public IFormFile File { get; set; } = null!;
+    }
+}
---
+++ src/Services/ProfileService/Profile.API/Features/CompanyProfiles/Commands/Validators/UploadCompanyLogoCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Profile.API.Features.CompanyProfiles.Commands.UploadLogo;
+
+namespace Profile.API.Features.CompanyProfiles.Commands.Validators
+{
+    public class UploadCompanyLogoCommandValidator : AbstractValidator<UploadCompanyLogoCommand>
+    {
+        public UploadCompanyLogoCommandValidator()
+        {
+            RuleFor(p => p.Id).NotEmpty().WithMessage("Id is required.");
+
+            RuleFor(p => p.File)
+                .Cascade(CascadeMode.Stop)
+                .NotNull().WithMessage("File is required.")
+                .Must(f => f.Length <= 5 * 1024 * 1024).WithMessage("File size must be less than 5MB.")
+                .Must(f => f.ContentType == "image/png" || f.ContentType == "image/jpg" || f.ContentType == "image/jpeg")
+                    .WithMessage("Only png/jpg/jpeg files are allowed for Logo upload.");
+        }
+    }
+}
