b974f618b9b7db654077b64a865868849fbdb93f
David Toholj <davidtoholj0101@gmail.com>
2026-04-19 17:18:53 +0200

    Add more checks for valid pdf/image file
    New checks added as extenstions for validator class.
    Refs #20

--- src/Services/ProfileService/Profile.API/Features/CompanyProfiles/Commands/Validators/UploadCompanyLogoCommandValidator.cs
+++ src/Services/ProfileService/Profile.API/Features/CompanyProfiles/Commands/Validators/UploadCompanyLogoCommandValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Profile.API.Extensions;
 using Profile.API.Features.CompanyProfiles.Commands.UploadLogo;
 
 namespace Profile.API.Features.CompanyProfiles.Commands.Validators
@@ -12,9 +13,9 @@ namespace Profile.API.Features.CompanyProfiles.Commands.Validators
             RuleFor(p => p.File)
                 .Cascade(CascadeMode.Stop)
                 .NotNull().WithMessage("File is required.")
+                .Must(f => f.Length > 0).WithMessage("File cannot be empty.")
                 .Must(f => f.Length <= 5 * 1024 * 1024).WithMessage("File size must be less than 5MB.")
-                .Must(f => f.ContentType == "image/png" || f.ContentType == "image/jpg" || f.ContentType == "image/jpeg")
-                    .WithMessage("Only png/jpg/jpeg files are allowed for Logo upload.");
+                .ValidImage();
         }
     }
 }
