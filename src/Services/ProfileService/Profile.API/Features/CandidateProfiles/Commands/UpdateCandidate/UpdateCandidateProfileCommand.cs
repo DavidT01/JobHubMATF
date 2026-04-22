746dc11debd273199fa764cef1acf920a5d73d13
David Toholj <davidtoholj0101@gmail.com>
2026-04-22 20:23:18 +0200

    Add profile picture url to `CandidateProfile` entity, DTO and commands
    Refs #31

--- src/Services/ProfileService/Profile.API/Features/CandidateProfiles/Commands/UpdateCandidate/UpdateCandidateProfileCommand.cs
+++ src/Services/ProfileService/Profile.API/Features/CandidateProfiles/Commands/UpdateCandidate/UpdateCandidateProfileCommand.cs
@@ -16,6 +16,7 @@ namespace Profile.API.Features.CandidateProfiles.Commands.UpdateCandidate
         public string Projects { get; set; } = string.Empty;
         public string Skills { get; set; } = string.Empty;
         public string Languages { get; set; } = string.Empty;
+        public string? PictureUrl { get; set; }
         public string CvUrl { get; set; } = string.Empty;
         public string? GithubUrl { get; set; }
         public string? GitlabUrl { get; set; }
