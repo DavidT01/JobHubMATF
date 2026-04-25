20a35a9cd4a9bd2bc369cb3220d60ed9d3b4f15b
David Toholj <davidtoholj0101@gmail.com>
2026-04-25 23:52:46 +0200

    Create new DTOs and update Mapper
    Refs #34

---
+++ src/Services/ProfileService/Profile.API/DTOs/LanguageDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Profile.API.DTOs
+{
+    public record LanguageDto(string Name, string Level);
+}
---
+++ src/Services/ProfileService/Profile.API/DTOs/ProjectDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Profile.API.DTOs
+{
+    public record ProjectDto(string Name, string Description, string RepositoryLink);
+}
