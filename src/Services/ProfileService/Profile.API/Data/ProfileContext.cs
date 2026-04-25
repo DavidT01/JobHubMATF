f24461bd97674f6319b5a1c0645cf14285001bcf
David Toholj <davidtoholj0101@gmail.com>
2026-04-25 23:43:49 +0200

    Configure data model
    Refs #34

--- src/Services/ProfileService/Profile.API/Data/ProfileContext.cs
+++ src/Services/ProfileService/Profile.API/Data/ProfileContext.cs
@@ -13,6 +13,10 @@ namespace Profile.API.Data
 
         public DbSet<CandidateProfile> CandidateProfiles { get; set; } = null!;
         public DbSet<CompanyProfile> CompanyProfiles { get; set; } = null!;
+        public DbSet<Education> Educations { get; set; } = null!;
+        public DbSet<Experience> Experiences { get; set; } = null!;
+        public DbSet<Project> Projects { get; set; } = null!;
+        public DbSet<Language> Languages { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -20,6 +24,30 @@ namespace Profile.API.Data
 
             modelBuilder.Entity<CandidateProfile>().HasIndex(p => p.UserId).IsUnique();
             modelBuilder.Entity<CompanyProfile>().HasIndex(p => p.UserId).IsUnique();
+
+            modelBuilder.Entity<CandidateProfile>()
+                .HasMany(c => c.Education)
+                .WithOne(e => e.CandidateProfile)
+                .HasForeignKey(e => e.CandidateProfileId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CandidateProfile>()
+                .HasMany(c => c.Experience)
+                .WithOne(e => e.CandidateProfile)
+                .HasForeignKey(e => e.CandidateProfileId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CandidateProfile>()
+                .HasMany(c => c.Projects)
+                .WithOne(p => p.CandidateProfile)
+                .HasForeignKey(p => p.CandidateProfileId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CandidateProfile>()
+                .HasMany(c => c.Languages)
+                .WithOne(l => l.CandidateProfile)
+                .HasForeignKey(l => l.CandidateProfileId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
