3c884a0e70d18fe3f3247315049fcb941f0ba561
David Toholj <davidtoholj0101@gmail.com>
2026-04-26 13:51:50 +0200

    Create new migration for profile db
    Refs #34

--- src/Services/ProfileService/Profile.API/Migrations/20260426114536_AddNewCandidateProfileCollections.Designer.cs
+++ src/Services/ProfileService/Profile.API/Migrations/20260426114536_AddNewCandidateProfileCollections.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Profile.API.Data;
@@ -12,9 +13,11 @@ using Profile.API.Data;
 namespace Profile.API.Migrations
 {
     [DbContext(typeof(ProfileContext))]
-    partial class ProfileContextModelSnapshot : ModelSnapshot
+    [Migration("20260426114536_AddNewCandidateProfileCollections")]
+    partial class AddNewCandidateProfileCollections
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
