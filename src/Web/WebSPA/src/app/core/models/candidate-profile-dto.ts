dcae18bf16ea8f05c98984ebb205cd3cbe24c55f
David Toholj <davidtoholj0101@gmail.com>
2026-04-28 22:20:11 +0200

    Update Angular DTOs for CandidateProfile
    Refs #21

--- src/Web/WebSPA/src/app/core/models/candidate-profile-dto.ts
+++ src/Web/WebSPA/src/app/core/models/candidate-profile-dto.ts
@@ -1,3 +1,8 @@
+import { EducationDto } from "./education-dto";
+import { ExperienceDto } from "./experience-dto";
+import { ProjectDto } from "./project-dto";
+import { LanguageDto } from "./language-dto";
+
 export interface CandidateProfileDto {
   id: string;
   userId: string;
@@ -6,11 +11,11 @@ export interface CandidateProfileDto {
   email: string;
   phoneNumber: string;
   location: string;
-  education: string;
-  experience: string;
-  projects: string;
-  skills: string;
-  languages: string;
+  education: EducationDto[];
+  experience: ExperienceDto[];
+  projects: ProjectDto[];
+  skills: string[];
+  languages: LanguageDto[];
   cvUrl: string;
   githubUrl?: string;
   gitlabUrl?: string;
