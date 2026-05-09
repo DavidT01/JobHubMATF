ee0cc7862b8266a7af56db29f82276b8288bb67d
David Toholj <davidtoholj0101@gmail.com>
2026-05-09 20:27:08 +0200

    Register route for company profile
    Refs #22

--- src/Web/WebSPA/src/app/app.routes.ts
+++ src/Web/WebSPA/src/app/app.routes.ts
@@ -5,5 +5,10 @@ export const routes: Routes = [
     path: 'profile/candidate/:userId',
     loadComponent: () => import('./components/candidate-profile/candidate-profile-component')
       .then(c => c.CandidateProfileComponent)
+  },
+  {
+    path: 'profile/company/:userId',
+    loadComponent: () => import('./components/company-profile/company-profile-component')
+      .then(c => c.CompanyProfileComponent)
   }
 ];
