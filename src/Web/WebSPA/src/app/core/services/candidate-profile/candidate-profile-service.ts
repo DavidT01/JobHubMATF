42b4490554951b4278f014e0ac8fe0b39af471da
David Toholj <davidtoholj0101@gmail.com>
2026-05-13 21:00:11 +0200

    Add cv and profile picture endpoints in candidate component
    Refs #23

--- src/Web/WebSPA/src/app/core/services/candidate-profile/candidate-profile-service.ts
+++ src/Web/WebSPA/src/app/core/services/candidate-profile/candidate-profile-service.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+
 import { CandidateProfileDto } from '../../models/candidate-profile-dto';
+import { UrlResponseDto } from '../../models/url-response-dto';
 
 @Injectable({
   providedIn: 'root',
@@ -21,5 +23,17 @@ export class CandidateProfileService {
 
   deleteProfile(id: string): Observable<void> {
     return this.http.delete<void>(`${this.api}/${id}`);
+  }
+
+  uploadCv(id: string, file: File): Observable<UrlResponseDto> {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<UrlResponseDto>('${this.api}/{id}/cv', formData);
+  }
+
+  uploadPicture(id: string, file: File): Observable<UrlResponseDto> {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<UrlResponseDto>('${this.api}/{id}/picture', formData);
   }
 }
