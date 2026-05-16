0cb87ea83731021a71426f9efca06eca872ac833
David Toholj <davidtoholj0101@gmail.com>
2026-05-16 22:22:21 +0200

    Add `RecruitmentProcess` and `SelectionRound` entities
    Refs #42

---
+++ src/Services/RecruitmentService/Recruitment.API/Entities/SelectionRound.cs
@@ -0,0 +1,15 @@
+﻿namespace Recruitment.API.Entities
+{
+    public class SelectionRound
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid RecruitmentProcessId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int Index { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? ModifiedAt { get; set; }
+
+        public RecruitmentProcess? RecruitmentProcess { get; set; }
+    }
+}
