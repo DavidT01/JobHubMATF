cb990c26730cd343b837d0799fad00d3f7ae938a
Petar <petarlukovic17@gmail.com>
2026-08-03 22:41:37 +0200

    fix(catalog): Add CORS policy for Angular dev server

--- src/Services/Catalog/Program.cs
+++ src/Services/Catalog/Program.cs
@@ -11,6 +11,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy => policy.WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +27,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowFrontend");
 
 app.MapControllers();
 
