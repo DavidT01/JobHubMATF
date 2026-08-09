747d1b878f564ded0769c9b8a220d7509599340a
David Toholj <davidtoholj0101@gmail.com>
2026-08-09 20:03:19 +0200

    Add GetProcessByJobId query and validation
    Refs #48

---
+++ src/Services/RecruitmentService/Recruitment.API/Features/Queries/GetProcessByJobId/GetProcessByJobIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Recruitment.API.Data;
+using Recruitment.API.DTOs;
+
+namespace Recruitment.API.Features.Queries.GetProcessByJobId
+{
+    public class GetProcessByJobIdQueryHandler(RecruitmentContext context, IMapper mapper, ILogger<GetProcessByJobIdQueryHandler> logger) : IRequestHandler<GetProcessByJobIdQuery, RecruitmentProcessDto?>
+    {
+        public async Task<RecruitmentProcessDto?> Handle(GetProcessByJobIdQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Pokrenut upit GetProcessByJobIdQuery za JobId: {JobId}", request.JobId);
+
+            var process = await context.Processes
+                .Include(p => p.Rounds.OrderBy(r => r.Index))
+                .FirstOrDefaultAsync(p => p.JobId == request.JobId, cancellationToken);
+
+            if (process == null)
+                return null;
+
+            return mapper.Map<RecruitmentProcessDto>(process);
+        }
+    }
+}
