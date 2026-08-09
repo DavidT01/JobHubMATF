06ed84c9a891eb82be3d0c98cb2141efebaf8255
David Toholj <davidtoholj0101@gmail.com>
2026-08-09 20:03:19 +0200

    Add GetProcessByJobId query and validation
    Refs #48

---
+++ src/Services/RecruitmentService/Recruitment.API/Exceptions/RecruitmentValidationException.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation.Results;
+
+namespace Recruitment.API.Exceptions
+{
+    public class RecruitmentValidationException(IEnumerable<ValidationFailure> errors) : Exception("Validation failed.")
+    {
+        public IEnumerable<ValidationFailure> Errors { get; } = errors;
+    }
+}
