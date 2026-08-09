31e96cf9ae205e42901aa61e92e9cc619eb9f351
David Toholj <davidtoholj0101@gmail.com>
2026-08-09 19:46:22 +0200

    Add DTO mapping
    Refs #48

---
+++ src/Services/RecruitmentService/Recruitment.API/Mapping/RecruitmentMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Recruitment.API.DTOs;
+using Recruitment.API.Entities;
+
+namespace Recruitment.API.Mapping
+{
+    public class RecruitmentMappingProfile : AutoMapper.Profile
+    {
+        public RecruitmentMappingProfile()
+        {
+            CreateMap<SelectionRound, SelectionRoundDto>();
+            CreateMap<RecruitmentProcess, RecruitmentProcessDto>();
+        }
+    }
+}
