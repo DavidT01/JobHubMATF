6076047c289c332d4ac3418a6dcbb9b83f2b3cae
David Toholj <davidtoholj0101@gmail.com>
2026-08-09 19:45:41 +0200

    Add Recruitment process and Selection round DTOs
    Refs #48

---
+++ src/Services/RecruitmentService/Recruitment.API/DTOs/RecruitmentProcessDto.cs
@@ -0,0 +1,13 @@
+﻿using Recruitment.API.Entities;
+
+namespace Recruitment.API.DTOs
+{
+    public class RecruitmentProcessDto
+    {
+        public Guid Id { get; set; }
+        public Guid CompanyId { get; set; }
+        public Guid JobId { get; set; }
+        public bool IsActive { get; set; }
+        public List<SelectionRound> Rounds { get; set; } = [];
+    }
+}
