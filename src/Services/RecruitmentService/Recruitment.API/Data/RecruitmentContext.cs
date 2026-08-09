ca6bb23b9417fdc4bcd62f7bb14d989dd474d28f
David Toholj <davidtoholj0101@gmail.com>
2026-08-09 17:54:12 +0200

    Setup DB context and connect postgresql
    Refs #42

---
+++ src/Services/RecruitmentService/Recruitment.API/Data/RecruitmentContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recruitment.API.Entities;
+
+namespace Recruitment.API.Data
+{
+    public class RecruitmentContext : DbContext
+    {
+        public RecruitmentContext(DbContextOptions<RecruitmentContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<RecruitmentProcess> Processes { get; set; }
+        public DbSet<SelectionRound> Rounds { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<SelectionRound>()
+                .HasOne(sr => sr.RecruitmentProcess)
+                .WithMany(rp => rp.Rounds)
+                .HasForeignKey(sr => sr.RecruitmentProcessId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
