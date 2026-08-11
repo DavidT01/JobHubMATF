ceb061d7754b266996e84cd51b53972a1c27bba4
David Toholj <davidtoholj0101@gmail.com>
2026-08-11 19:43:10 +0200

    Implement recruitment proces Angular service and DTOs
    Refs #51

---
+++ src/Web/WebSPA/src/app/core/services/recruitment-process/recruitment-process-service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+
+import { RecruitmentProcessDto } from '../../models/recruitment-process-dto';
+import { SelectionRoundDto } from '../../models/selection-round-dto';
+import { CreateProcessCommandDto } from '../../models/create-process-command-dto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecruitmentProcessService {
+  private apiUrl = `${environment.apiUrl}/recruitment-processes`;
+
+  constructor(private http: HttpClient) { }
+
+  getProcessByJobId(jobId: string): Observable<RecruitmentProcessDto> {
+    return this.http.get<RecruitmentProcessDto>(`${this.apiUrl}/job/${jobId}`);
+  }
+
+  createProcess(command: CreateProcessCommandDto): Observable<string> {
+    return this.http.post<string>(this.apiUrl, command);
+  }
+
+  updateRounds(processId: string, rounds: SelectionRoundDto[]): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${processId}/rounds`, rounds);
+  }
+}
