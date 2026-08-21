1a46c813bff769bef97fcf2f10c3a2e2a315fa85
Petar <petarlukovic17@gmail.com>
2026-08-21 19:57:21 +0200

    feat(catalog-ui): add JobList component

--- src/Web/WebSPA/src/app/app.routes.ts
+++ src/Web/WebSPA/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { JobList } from './components/job-list/job-list';
+import { JobSearch } from './components/job-search/job-search';
+import { JobDetails } from './components/job-details/job-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '' , component: JobList},
+    {path: 'search' , component: JobSearch},
+    {path: 'jobs/:id' , component: JobDetails}
+];
