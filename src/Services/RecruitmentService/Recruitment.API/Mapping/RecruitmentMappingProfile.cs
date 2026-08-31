1b0c2ea05268ffe4a3831d60a9f0284f754ee662
David Toholj <davidtoholj0101@gmail.com>
2026-08-31 20:15:17 +0200

    Update recruitment mapping profile
    Refs #57

--- src/Services/RecruitmentService/Recruitment.API/Mapping/RecruitmentMappingProfile.cs
+++ src/Services/RecruitmentService/Recruitment.API/Mapping/RecruitmentMappingProfile.cs
@@ -3,6 +3,8 @@ using Recruitment.API.DTOs;
 using Recruitment.API.Entities;
 using Recruitment.API.Features.Commands.CreateRecruitmentProcess;
 using Recruitment.API.Features.Commands.ScheduleInterview;
+using Recruitment.API.Features.Commands.EvaluateCandidate;
+using Recruitment.API.Features.Commands.AdvanceCandidate;
 
 namespace Recruitment.API.Mapping
 {
@@ -17,6 +19,12 @@ namespace Recruitment.API.Mapping
 
             CreateMap<ScheduleInterviewCommand, InterviewSchedule>();
             CreateMap<InterviewSchedule, InterviewScheduleDto>();
+
+            CreateMap<CandidateEvaluation, CandidateEvaluationDto>();
+            CreateMap<EvaluateCandidateCommand, CandidateEvaluation>();
+
+            CreateMap<CandidateProgress, CandidateProgressDto>();
+            CreateMap<AdvanceCandidateCommand, CandidateProgress>();
         }
     }
 }
