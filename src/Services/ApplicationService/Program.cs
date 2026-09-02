85c7b402a10cc3604931d614db9d94159ffae42b
mi16087 <mihajlozivkovic97@gmail.com>
2026-09-02 20:29:58 +0200

    Add application error handling

--- src/Services/ApplicationService/Program.cs
+++ src/Services/ApplicationService/Program.cs
@@ -1,16 +1,29 @@
 using ApplicationService.Application;
 using ApplicationService.Infrastructure;
+using ApplicationService.Infrastructure.Errors;
 using ApplicationService.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddOpenApi();
+builder.Services.AddProblemDetails(options =>
+{
+    options.CustomizeProblemDetails = context =>
+    {
+        context.ProblemDetails.Instance = context.HttpContext.Request.Path;
+        context.ProblemDetails.Extensions["traceId"] = context.HttpContext.TraceIdentifier;
+    };
+});
+builder.Services.AddExceptionHandler<ApplicationExceptionHandler>();
 builder.Services.AddApplicationLayer(builder.Configuration);
 builder.Services.AddApplicationInfrastructure();
 builder.Services.AddApplicationPersistence(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseExceptionHandler();
+app.UseStatusCodePages();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
