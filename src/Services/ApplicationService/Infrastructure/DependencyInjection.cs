6a0e355fdbee448944d22bf5a9e9d40b5a561029
mi16087 <mihajlozivkovic97@gmail.com>
2026-09-02 14:38:27 +0200

    Configure service dependencies

---
+++ src/Services/ApplicationService/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,10 @@
+namespace ApplicationService.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationInfrastructure(
+        this IServiceCollection services)
+    {
+        return services;
+    }
+}
