12019a1380968b33c18a8922c5cc2275661b4181
David Toholj <davidtoholj0101@gmail.com>
2026-09-03 23:08:42 +0200

    Add Moq package and implement new test helpers
    Refs #68

--- src/Services/ProfileService/Profile.UnitTests/Common/TestHelpers.cs
+++ src/Services/ProfileService/Profile.UnitTests/Common/TestHelpers.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging.Abstractions;
 using Profile.API.Data;
 using Profile.API.Mapping;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Moq;
 
 namespace Profile.UnitTests.Common
 {
@@ -26,6 +29,48 @@ namespace Profile.UnitTests.Common
             }, NullLoggerFactory.Instance);
 
             return configuration.CreateMapper();
+        }
+
+        public static IFormFile CreateFile(string fileName, string contentType, string content)
+        {
+            return CreateFile(fileName, contentType, System.Text.Encoding.UTF8.GetBytes(content));
+        }
+
+        public static FormFile CreateFile(string fileName, string contentType, byte[] content)
+        {
+            var stream = new MemoryStream(content);
+            return new FormFile(stream, 0, stream.Length, "file", fileName)
+            {
+                Headers = new HeaderDictionary { ["Content-Type"] = contentType }
+            };
+        }
+
+        public static IWebHostEnvironment CreateEnvironment(string? rootPath)
+        {
+            var environment = new Mock<IWebHostEnvironment>();
+            environment.SetupGet(item => item.WebRootPath).Returns(rootPath!);
+            return environment.Object;
+        }
+
+        public static TemporaryRoot CreateTemporaryRoot()
+        {
+            return new TemporaryRoot();
+        }
+
+        public sealed class TemporaryRoot : IDisposable
+        {
+            public TemporaryRoot()
+            {
+                Path = System.IO.Path.Combine(System.IO.Path.GetTempPath(), Guid.NewGuid().ToString());
+                Directory.CreateDirectory(Path);
+            }
+
+            public string Path { get; }
+
+            public void Dispose()
+            {
+                Directory.Delete(Path, true);
+            }
         }
     }
 }
