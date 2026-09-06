bf01eb170eaf2b315c206e86c5ea92b2825bb2c0
David Toholj <davidtoholj0101@gmail.com>
2026-09-06 12:13:09 +0200

    Expose new interview management endpoints
    Refs #85

--- src/Services/RecruitmentService/Recruitment.API/Controllers/InterviewController.cs
+++ src/Services/RecruitmentService/Recruitment.API/Controllers/InterviewController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Recruitment.API.Features.Commands.ScheduleInterview;
 using Recruitment.API.DTOs;
 using Recruitment.API.Features.Queries.GetInterviewSchedule;
+using Recruitment.API.Features.Commands.UpdateInterviewSchedule;
+using Recruitment.API.Features.Commands.CancelInterviewSchedule;
 
 namespace Recruitment.API.Controllers
 {
@@ -31,6 +33,27 @@ namespace Recruitment.API.Controllers
             logger.LogInformation("Received GetInterviewSchedule request for candidate {CandidateProfileId} and round {SelectionRoundId}", candidateProfileId, selectionRoundId);
             var result = await _mediator.Send(new GetInterviewScheduleQuery(candidateProfileId, selectionRoundId));
             return result is null ? NotFound() : Ok(result);
+        }
+
+        [HttpPut("{interviewScheduleId}")]
+        [ProducesResponseType(typeof(InterviewScheduleDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateInterviewSchedule(Guid interviewScheduleId, [FromBody] UpdateInterviewScheduleCommand command)
+        {
+            logger.LogInformation("Received UpdateInterviewSchedule request for interview {InterviewScheduleId}", interviewScheduleId);
+            var result = await _mediator.Send(command with { InterviewScheduleId = interviewScheduleId });
+            return Ok(result);
+        }
+
+        [HttpDelete("{interviewScheduleId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> CancelInterviewSchedule(Guid interviewScheduleId)
+        {
+            logger.LogInformation("Received CancelInterviewSchedule request for interview {InterviewScheduleId}", interviewScheduleId);
+            await _mediator.Send(new CancelInterviewScheduleCommand(interviewScheduleId));
+            return NoContent();
         }
     }
 }
