d3b47277567c8944b39ab0426c9077bc34c55e51
David Toholj <davidtoholj0101@gmail.com>
2026-09-06 13:01:05 +0200

    Centralize email validation
    Refs #85

--- src/Services/RecruitmentService/Recruitment.API/Features/Commands/Validators/ScheduleInterviewCommandValidator.cs
+++ src/Services/RecruitmentService/Recruitment.API/Features/Commands/Validators/ScheduleInterviewCommandValidator.cs
@@ -1,6 +1,5 @@
 using FluentValidation;
 using Recruitment.API.Features.Commands.ScheduleInterview;
-using System.Net.Mail;
 
 namespace Recruitment.API.Features.Commands.Validators;
 
@@ -12,20 +11,7 @@ public class ScheduleInterviewCommandValidator : AbstractValidator<ScheduleInter
         RuleFor(command => command.CandidateProfileId).NotEmpty();
         RuleFor(command => command.Title).NotEmpty().MaximumLength(200);
         RuleFor(command => command.StartTime).LessThan(command => command.EndTime);
-        RuleForEach(command => command.AttendeeEmails).Must(BeValidEmail)
+        RuleForEach(command => command.AttendeeEmails).Must(EmailValidation.IsValidEmail)
             .WithMessage("Each attendee email must be valid.");
-    }
-
-    private static bool BeValidEmail(string email)
-    {
-        try
-        {
-            _ = new MailAddress(email);
-            return true;
-        }
-        catch (FormatException)
-        {
-            return false;
-        }
     }
 }
--- src/Services/RecruitmentService/Recruitment.API/Features/Commands/Validators/UpdateInterviewScheduleCommandValidator.cs
+++ src/Services/RecruitmentService/Recruitment.API/Features/Commands/Validators/UpdateInterviewScheduleCommandValidator.cs
@@ -1,6 +1,5 @@
 using FluentValidation;
 using Recruitment.API.Features.Commands.UpdateInterviewSchedule;
-using System.Net.Mail;
 
 namespace Recruitment.API.Features.Commands.Validators;
 
@@ -11,20 +10,7 @@ public class UpdateInterviewScheduleCommandValidator : AbstractValidator<UpdateI
         RuleFor(command => command.InterviewScheduleId).NotEmpty();
         RuleFor(command => command.Title).NotEmpty().MaximumLength(200);
         RuleFor(command => command.StartTime).LessThan(command => command.EndTime);
-        RuleForEach(command => command.AdditionalAttendeeEmails).Must(BeValidEmail)
+        RuleForEach(command => command.AdditionalAttendeeEmails).Must(EmailValidation.IsValidEmail)
             .WithMessage("Each attendee email must be valid.");
-    }
-
-    private static bool BeValidEmail(string email)
-    {
-        try
-        {
-            _ = new MailAddress(email);
-            return true;
-        }
-        catch (FormatException)
-        {
-            return false;
-        }
     }
 }
