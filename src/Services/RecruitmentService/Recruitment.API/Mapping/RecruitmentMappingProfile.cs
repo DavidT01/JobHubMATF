6c5661c311eeca774485f83fa4f22914a34b0462
David Toholj <davidtoholj0101@gmail.com>
2026-09-06 09:24:24 +0200

    Update entity, DTO and mapping to support updating and deleting existing interview schedules
    Refs #85

--- src/Services/RecruitmentService/Recruitment.API/Mapping/RecruitmentMappingProfile.cs
+++ src/Services/RecruitmentService/Recruitment.API/Mapping/RecruitmentMappingProfile.cs
@@ -5,6 +5,7 @@ using Recruitment.API.Features.Commands.CreateRecruitmentProcess;
 using Recruitment.API.Features.Commands.ScheduleInterview;
 using Recruitment.API.Features.Commands.EvaluateCandidate;
 using Recruitment.API.Features.Commands.AdvanceCandidate;
+using Recruitment.API.Features.Commands.UpdateInterviewSchedule;
 
 namespace Recruitment.API.Mapping
 {
@@ -19,7 +20,15 @@ namespace Recruitment.API.Mapping
             CreateMap<SelectionRoundDto, SelectionRound>().ForMember(dest => dest.Index, opt => opt.MapFrom(src => src.OrderIndex));
             CreateMap<SelectionRoundInsertDto, SelectionRound>().ForMember(dest => dest.Index, opt => opt.MapFrom(src => src.OrderIndex));
 
-            CreateMap<ScheduleInterviewCommand, InterviewSchedule>();
+            CreateMap<ScheduleInterviewCommand, InterviewSchedule>()
+                .ForMember(dest => dest.AdditionalAttendeeEmails, opt => opt.MapFrom(src => src.AttendeeEmails));
+            CreateMap<UpdateInterviewScheduleCommand, InterviewSchedule>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.SelectionRoundId, opt => opt.Ignore())
+                .ForMember(dest => dest.CandidateProfileId, opt => opt.Ignore())
+                .ForMember(dest => dest.EventId, opt => opt.Ignore())
+                .ForMember(dest => dest.GoogleMeetUrl, opt => opt.Ignore())
+                .ForMember(dest => dest.ModifiedAt, opt => opt.Ignore());
             CreateMap<InterviewSchedule, InterviewScheduleDto>();
 
             CreateMap<CandidateEvaluation, CandidateEvaluationDto>();
